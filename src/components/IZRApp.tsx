18c0843a6a488fd024c740efab8c666052768959
medaminbl97 <mbellil262@gmail.com>
2025-02-12 07:29:30 +0000

    uodate izr app update buttons

--- src/components/IZRApp.tsx
+++ src/components/IZRApp.tsx
@@ -1,8 +1,8 @@
 
-import { Heading, Image, VStack, Text, Icon, Link, Stack } from '@chakra-ui/react'
+import { Heading, Image, VStack, Text, Link, Stack } from '@chakra-ui/react'
 import izr_app from "../assets/IZR_app_werbung.png"
-import { FaAndroid, FaApple } from 'react-icons/fa'
-import { colors } from '../theme'
+import google_play from "../assets/Google_Play_Store_badge.png"
+import app_store from "../assets/Download_on_the_App_Store_Badge.png"
 import useResponsiveBreakpoints from '../hooks/useResponsiveBreakpoints'
 function IZRApp() {
     const { isMobile } = useResponsiveBreakpoints();
@@ -11,14 +11,14 @@ function IZRApp() {
             <Heading width={"100%"}><Text>IZR APP herunterladen !</Text></Heading>
             <Stack flexDir={isMobile ? "column" : "row"} alignItems={"center"} justifyContent={"center"} width={"100%"} gap={"20%"}>
                 <Image src={izr_app} width={isMobile ? "50%" : "30%"} ></Image>
-                <Stack justifyContent={"center"} flexDir={isMobile ? "row" : "column"}>
+                <VStack marginY={10} width={isMobile ? "50%" : "30%"} justifyContent={"center"}>
                     <Link href="https://play.google.com/store/apps/details?id=com.aminbl.izrApp&pcampaignid=web_share" isExternal>
-                        <Icon as={FaAndroid} boxSize={20} color={colors.primary} _hover={{ opacity: 0.8 }} />
+                        <Image src={google_play}></Image>
                     </Link>
                     <Link href="https://apps.apple.com/de/app/izr/id6470660577?l" isExternal>
-                        <Icon as={FaApple} boxSize={20} color={colors.primary} _hover={{ opacity: 0.8 }} />
+                        <Image src={app_store}></Image>
                     </Link>
-                </Stack>
+                </VStack>
             </Stack>
         </VStack>
     )
